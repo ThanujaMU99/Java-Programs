05db9aa1a46eca9e76c63f1e1525f952c0068058
ThanujaMU99 <thanujamu.xworkz@gmai.com>
2021-09-07 23:04:52 +0530

    added

---
+++ Human.java
@@ -0,0 +1,15 @@
+class Human{
+
+public static void main(String a[]){
+
+      String Places="B";
+      int age=12;
+      boolean isAlive=true;
+System.out.println(Places);
+System.out.println(age);
+System.out.println(isAlive);
+
+
+}
+
+}
