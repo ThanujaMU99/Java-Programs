90d171dab53ff6802acaa0bd63ea926b489b8a20
ThanujaMU99 <thanujamu.xworkz@gmai.com>
2021-09-22 10:52:31 +0530

    File

---
+++ FilmIndustry.java
@@ -0,0 +1,14 @@
+class FlimIndustry{
+
+public String nameOfActor;
+public String nameOfProducers;
+public long costForMovie;
+
+
+public void movie(){
+
+System.out.println(" nice to watch");
+
+}
+
+}
